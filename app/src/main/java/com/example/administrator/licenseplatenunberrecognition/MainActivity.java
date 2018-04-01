2d40024b96abb158393227e878e59f05e692d311
mingyuesang1 <mingyue_sang@qq.com>
2018-04-01 19:36:08 +0800

    add tesseract

--- app/src/main/java/com/example/administrator/licenseplatenunberrecognition/MainActivity.java
+++ app/src/main/java/com/example/administrator/licenseplatenunberrecognition/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.administrator.licenseplatenunberrecognition;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -9,6 +10,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,6 +21,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        final String path = Environment.getExternalStorageDirectory().getAbsolutePath() + File.separator + "/LPNR/";
+        File destDir = new File(path);
+        if (!destDir.exists()) {
+            destDir.mkdirs();
+        }
+        destDir=new File(path+"picture/");
+        if (!destDir.exists()) {
+            destDir.mkdirs();
+        }
 
 
     }
